1b86b73ac6a478be6a344fcaea5b7131f570a8b8
dni <dni.khr@gmail.com>
2022-04-20 23:10:55 +0200

    first rpc call ;)

--- src/balancing.rs
+++ src/balancing.rs
@@ -1,8 +1,18 @@
-use cln_plugin::{Plugin, Error};
+use cln_plugin::{anyhow, Plugin, Error};
+use cln_rpc::{model::GetinfoRequest, ClnRpc, Request};
+
 use crate::PluginState;
 pub async fn balance_method(_p: Plugin<PluginState>, _v: serde_json::Value) -> Result<serde_json::Value, Error> {
     log::info!("Got a balance_method call: {}", _v);
-    let param = _p.option("test-option");
-    let message = format!("balancing... {:?}, {}", param.unwrap(), _v);
-    Ok(json!(message))
+
+    // let param = _p.option("test-option");
+    // let message = format!("balancing... {:?}, {}, {:?}", param.unwrap(), _v, _p.state().rpc_path);
+    // Ok(json!(message))
+
+    let mut rpc = ClnRpc::new(&_p.state().rpc_path).await?;
+    let response = rpc
+        .call(Request::Getinfo(GetinfoRequest {}))
+        .await
+        .map_err(|e| anyhow!("Error calling getinfo: {:?}", e))?;
+    Ok(json!(response))
 }
