d25adbe883c6e3cc8bae3a2363f44ca8c16f1646
dni <dni.khr@gmail.com>
2022-04-21 14:47:41 +0200

    iterating the response :)

--- src/balancing.rs
+++ src/balancing.rs
@@ -1,5 +1,5 @@
 use cln_plugin::{anyhow, Plugin, Error};
-use cln_rpc::{model::ListchannelsRequest, ClnRpc, Request};
+use cln_rpc::{model::ListpeersRequest, ClnRpc, Request, Response};
 use crate::PluginState;
 use serde::{Serialize, Deserialize};
 
@@ -12,10 +12,6 @@ struct Channel {
 }
 
 impl Channel {
-    // async fn new() -> Result<Channel, Error> {
-    //     self.short_id = "lololol".to_string();
-    //     Ok(self)
-    // }
     fn calculate_ratio(&self) -> f64 {
         self.spendable_msatoshis as f64 / self.receivable_msatoshis as f64
     }
@@ -24,8 +20,6 @@ impl Channel {
 pub async fn balance_method(_p: Plugin<PluginState>, _v: serde_json::Value) -> Result<serde_json::Value, Error> {
     log::info!("Got a balance_method call: {}", _v);
     // let param = _p.option("test-option");
-    // let message = format!("balancing... {:?}, {}, {:?}", param.unwrap(), _v, _p.state().rpc_path);
-    // Ok(json!(message))
 
     let rpc = ClnRpc::new(&_p.state().rpc_path).await?;
     let channels = get_balance_status(rpc);
@@ -34,21 +28,29 @@ pub async fn balance_method(_p: Plugin<PluginState>, _v: serde_json::Value) -> R
 
 async fn get_balance_status(mut rpc: ClnRpc) -> Result<Vec<Channel>, Error> {
     let response = rpc
-        .call(Request::ListChannels(ListchannelsRequest {
-          short_channel_id: None,
-          source: None,
-          destination: None,
+        .call(Request::ListPeers(ListpeersRequest {
+            id: None,
+            level: None,
         }))
         .await
-        .map_err(|e| anyhow!("Error calling listchannels: {:?}", e))?;
+        .map_err(|e| anyhow!("Error calling listpeers: {:?}", e))?;
 
-    let mut channels = vec!();
-    let channel = Channel{
-        alias: "lol".to_string(),
-        short_id: "lol".to_string(),
-        spendable_msatoshis: 1000,
-        receivable_msatoshis: 1000,
-    };
-    channels.push(channel);
-    Ok(channels)
+    match response {
+        Response::ListPeers(res) => {
+            log::info!("inside match: {:?}", &res.peers);
+            log::info!("inside 2 match: {:?}", &res.peers);
+            let mut channels: Vec<Channel> = vec!();
+            for peer in &res.peers {
+                let channel = Channel{
+                    alias: "lol".to_string(),
+                    short_id: "lol".to_string(),
+                    spendable_msatoshis: 1000,
+                    receivable_msatoshis: 1000,
+                };
+                channels.push(channel);
+            }
+            Ok(channels)
+        },
+        _ => Err(anyhow!("Response not ListPeers")),
+    }
 }
